7101e9253854a0ce0c8ed306cb039b4aeb3486fc
SubinHB <mohamedsubin1aa@gmail.com>
2021-07-19 22:44:46 +0530

    First copy

---
+++ src/test/java/com/stepdefinition/LoginPage.java
@@ -0,0 +1,28 @@
+package com.stepdefinition;
+
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class LoginPage {
+	
+	@Given("Open facebook page")
+	public void open_facebook_page() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new cucumber.api.PendingException();
+	}
+
+	@When("Enter username and password")
+	public void enter_username_and_password() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new cucumber.api.PendingException();
+	}
+
+	@Then("Click login button")
+	public void click_login_button() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new cucumber.api.PendingException();
+	}
+
+
+}
---
+++ src/test/resources/Features/LoginPage.feature
@@ -0,0 +1,7 @@
+Feature: Verifying the facebook details
+  Scenario: Verifying login with invalid credentials
+     Given Open facebook page
+     When Enter username and password
+     Then Click login button
+     
+     
